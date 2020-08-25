d8f2127018d0086129ac3c6043b7375db2c6ca10
mikemccaffrey <github@mikemccaffrey.net>
2020-08-25 13:48:51 -0700

    Reorder javascript for clarity.

--- src/js/theme.js
+++ src/js/theme.js
@@ -2,7 +2,9 @@
   Drupal.behaviors.themename = {
     attach: function (context, settings) {
 
-      // Toggle the site header mobile buttons.
+      //
+      // The mobile header menu and search toggles.
+      //
       $('.site-header__toggle').once('site-header-toggle').on('click', function(event) {
         // Check if this button is currently marked as expanded.
         var currentlyExpanded = $(this).attr('aria-expanded');
@@ -15,22 +17,21 @@
       });
       // Since javascript is working, toggle the mobile nav closed on page load.
       $('.site-header__toggle').attr('aria-expanded', 'false');
+      // Close the mobile header panes if the window is resized.
+      $(window).once('window-resize-mobile').resize(function () {
+        $('.site-header__toggle').attr('aria-expanded', 'false');
+      });
 
-      // Actions taken when the window changes sizes.
-      $(window).once('window-resize').resize(
-        // Use debounce to only run the resizing functions every 50ms.
-        Drupal.debounce(function () {
-          // Hide the drupal admin toolbar drawer when resizing to mobile, and show it when resizing to desktop.
-          if ($(window).width() < 1070) {
-            $('#toolbar-item-administration.is-active').click();
-          } else {
-            $('#toolbar-item-administration:not(.is-active)').click();
-          }
-          // Close the mobile header panes if the window is resized.
-          $('.site-header__toggle').attr('aria-expanded', 'false');
-        }, 50)
-      );
-
+      //
+      // Hide the drupal admin toolbar drawer when resizing to mobile, and show it when resizing to desktop.
+      //
+      $(window).once('window-resize-toolbar').resize(Drupal.debounce(function () {
+        if ($(window).width() < 1070) {
+          $('#toolbar-item-administration.is-active').click();
+        } else {
+          $('#toolbar-item-administration:not(.is-active)').click();
+        }
+      }, 50));
     }
   };
 })(jQuery, Drupal);
