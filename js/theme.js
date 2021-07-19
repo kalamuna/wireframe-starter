b5933e6eeec3e8024cdd473c7910356de00702d3
mikemccaffrey <github@mikemccaffrey.net>
2021-07-19 10:13:58 -0700

    Switch to menu-item-toggle for both versions of menu.

--- js/theme.js
+++ js/theme.js
@@ -8,7 +8,7 @@
       $('#region--header-menu nav > ul > li > ul, #region--mobile-menus nav > ul > li > ul').once('mobile-menus-toggle').each(function() {
         var id = Math.random().toString(36).substring(2, 15);
         $(this).attr('id', 'menu-item-' + id)
-        $(this).before('<button class="mobile-menus-toggle" aria-controls="menu-item-' + id + '" aria-expanded="false"><span class="visually-hidden">Toggle submenu</span><svg style="max-width:1em; max-height: 1em;" aria-hidden="true"><use xlink:href="#symbol-open" xmlns:xlink="http://www.w3.org/1999/xlink"></use></svg></button>');
+        $(this).before('<button class="menu-item-toggle" aria-controls="menu-item-' + id + '" aria-expanded="false"><span class="visually-hidden">Toggle submenu</span><svg style="max-width:1em; max-height: 1em;" aria-hidden="true"><use xlink:href="#symbol-open" xmlns:xlink="http://www.w3.org/1999/xlink"></use></svg></button>');
       });
 
 
